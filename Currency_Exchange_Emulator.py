a9496aa7eff24d5d94a4e90ec751ebd335dd8a57
tamccullough88 <tamccullough88@gmail.com>
2024-02-21 18:38:15 -0500

    first commit

---
+++ Currency_Exchange_Emulator.py
@@ -0,0 +1,66 @@
+class Currency:
+
+    currencies =  {
+        'CHF': 0.930023, #swiss franc 
+        'CAD': 1.264553, #canadian dollar
+        'GBP': 0.737414, #british pound
+        'JPY': 111.019919, #japanese yen
+        'EUR': 0.862361, #euro
+        'USD': 1.0 #us dollar
+        } 
+      
+    def __init__(self, value, unit="USD"):
+        self.value = value
+        self.unit = unit
+
+    def changeTo(self, new_unit):
+        """
+        An Currency object is transformed from the unit "self.unit" to "new_unit"
+        """
+        self.value = (self.value / Currency.currencies[self.unit] * Currency.currencies[new_unit])
+        self.unit = new_unit
+
+#add magic methods here
+    def __repr__(self):
+# This method returns the string to be printed. This should be the value rounded to two digits, accompanied by its acronym.
+        return f"{round(self.value,2)} {self.unit}"
+
+    def __str__(self):
+    #This method returns the same value as __repr__(self).
+        return f"{round(self.value,2)} {self.unit}"
+
+    def __add__(self,other):
+        #Defines the '+' operator. If other is a Currency object, the currency values are added and the result will be the unit of self. If other is an int or a float, other will be treated as a USD value.
+        if type(other) == int or type(other) == float:
+            x = (other * Currency.currencies[self.unit])
+        else:
+            x = (other.value / Currency.currencies[other.unit] * Currency.currencies[self.unit]) 
+        return Currency(x + self.value, self.unit)
+    
+    def __iadd__(self, other):
+        return Currency.__add__(self,other)
+    
+    def __radd__(self,other):
+        res = self + other
+        return res
+        
+
+    def __sub__(self,other):
+        if type(other) == int or type(other) == float:
+            x = (other * Currency.currencies[self.unit])
+        else:
+            x = (other.value / Currency.currencies[other.unit] * Currency.currencies[self.unit]) 
+        return Currency(self.value - x, self.unit)            
+        
+    
+
+v1 = Currency(23.43, "EUR")
+v2 = Currency(19.97, "USD")
+print(f"1",v1 + v2)
+print(f"2",v2 + v1)
+print(f"3",v1 + 3) # an int or a float is considered to be a USD value
+print(f"4",7.81 + v1)
+print(f"5",v1 - 3) # an int or a float is considered to be a USD value
+print(f"6",v2 - 30) 
+
+
